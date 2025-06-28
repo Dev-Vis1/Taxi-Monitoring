e47af9de2795eba9bfa4ae395a6c37eb5a47757b
Matthew Ayodele <matthew.ayodele@tuhh.de>
2025-06-28 16:15:34 +0200

    fixed run time error: deque mutated during iteratioin

--- dash-app/app.py
+++ dash-app/app.py
@@ -496,8 +496,9 @@ def update_dashboard(n, zoom_in, zoom_out, reset_view, selected_taxi_id, relayou
     
     # Create incident cards
     incident_cards = []
-    if incident_log:
-        for incident in incident_log:
+    incidents_snapshot = list(incident_log)
+    if incidents_snapshot:
+        for incident in incidents_snapshot:
             card = dbc.Card(
                 dbc.CardBody([
                     html.Div([
